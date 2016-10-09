2f1329d8c4633c39e1335d438042519991ed0a77
Lan5432 <lan5432@hotmail.com>
2016-10-09 16:25:23 +0200

    En el archivo está todo lo que hay que ejecutar para recrear la base.
    Abajo están los comandos GRANT que ya veremos si funcionan. De momento me falta un UO

--- IPS-Project/Others/EsquemaBD/DDL.sql
+++ IPS-Project/Others/EsquemaBD/DDL.sql
@@ -27,10 +27,10 @@ CREATE TABLE Producto(
 	idProducto VARCHAR (20),
 	producto_nombre VARCHAR(50),
 	descripcion_producto VARCHAR(500),
-	stock NUMERIC(4, 0),
-	precio NUMERIC (4, 2),
+	stock DECIMAL(4, 0),
+	precio DECIMAL (6, 2),
 	
-	PRIMARY KEY idProducto
+	PRIMARY KEY(idProducto)
 );
 
 CREATE TABLE ProductoPedido(
@@ -56,16 +56,20 @@ INSERT INTO Almacenero VALUES ('alm01', 'cntrsAlmacenero01', 'Almacenero01', 'Al
 INSERT INTO Almacenero VALUES ('alm02', 'cntrsAlmacenero02', 'Almacenero02', 'AlmaceneroApellido02');
 INSERT INTO Almacenero VALUES ('alm03', 'cntrsAlmacenero03', 'Almacenero03', 'AlmaceneroApellido03');
 
-INSERT INTO Producto VALUES ('prod01', 'Tobias silla', ' Gracias a la flexibilidad del asiento y el respaldo, resulta muy cómoda.', 50, 100);
-INSERT INTO Producto VALUES ('prod02',  'Gregor silla', ' La altura de la silla se puede regular y te ofrece la máxima comodidad', 20, 90);
-INSERT INTO Producto VALUES ('prod03',  'Orfjall silla', 'Gracias a la espuma de alta densidad, la silla resultará cómoda durante muchos años.', 55, 110);
-INSERT INTO Producto VALUES ('prod04',  'Orfjall / Sporren silla', 'Gracias a la espuma de alta densidad, la silla resultará cómoda durante muchos años.', 50, 100);
-INSERT INTO Producto VALUES ('prod05',  'Molten silla', 'Con soporte lumbar para que tu espalda tenga más superficie de descanso y apoyo.', 10, 500);
-INSERT INTO Producto VALUES ('prod06',  'Vagsberg silla', ' La altura de la silla se puede regular y te ofrece la máxima comodidad', 50, 100);
-INSERT INTO Producto VALUES ('prod07',  'Jules silla', ' La altura de la silla se puede regular y te ofrece la máxima comodidad.', 50, 100);
+INSERT INTO Producto VALUES ('prod01', 'Tobias silla', 'Gracias a la flexibilidad del asiento y el respaldo, resulta muy cómoda.', 50, 100.00);
+INSERT INTO Producto VALUES ('prod02',  'Gregor silla', 'La altura de la silla se puede regular y te ofrece la máxima comodidad', 20, 90.00);
+INSERT INTO Producto VALUES ('prod03',  'Orfjall silla', 'Gracias a la espuma de alta densidad, la silla resultará cómoda durante muchos años.', 55, 110.00);
+INSERT INTO Producto VALUES ('prod04',  'Orfjall / Sporren silla', 'Gracias a la espuma de alta densidad, la silla resultará cómoda durante muchos años.', 50, 100.00);
+INSERT INTO Producto VALUES ('prod05',  'Molten silla', 'Con soporte lumbar para que tu espalda tenga más superficie de descanso y apoyo.', 10, 500.00);
+INSERT INTO Producto VALUES ('prod06',  'Vagsberg silla', 'La altura de la silla se puede regular y te ofrece la máxima comodidad', 50, 100.00);
+INSERT INTO Producto VALUES ('prod07',  'Jules silla', 'La altura de la silla se puede regular y te ofrece la máxima comodidad.', 50, 100.00);
 
-INSERT INTO Producto VALUES ('prod08',  'Orjfall / Numben silla', ' Gracias a la flexibilidad del asiento y el respaldo, resulta muy cómoda.', 10, 200);
-INSERT INTO Producto VALUES ('prod09',  'Vilmar silla', ' Gracias a la flexibilidad del asiento y el respaldo, resulta muy cómoda.', 54, 10);
-INSERT INTO Producto VALUES ('prod10',  'Frode silla', ' Gracias a la flexibilidad del asiento y el respaldo, resulta muy cómoda.', 70, 100);
-INSERT INTO Producto VALUES ('prod11',  'Janinge silla', ' Gracias a la flexibilidad del asiento y el respaldo, resulta muy cómoda.', 20, 100);
-INSERT INTO Producto VALUES ('prod12',  'Tobias silla', ' It just works!', 50, 100);
+INSERT INTO Producto VALUES ('prod08',  'Orjfall / Numben silla', 'Gracias a la flexibilidad del asiento y el respaldo, resulta muy cómoda.', 10, 200.00);
+INSERT INTO Producto VALUES ('prod09',  'Vilmar silla', 'Gracias a la flexibilidad del asiento y el respaldo, resulta muy cómoda.', 54, 10.00);
+INSERT INTO Producto VALUES ('prod10',  'Frode silla', 'Gracias a la flexibilidad del asiento y el respaldo, resulta muy cómoda.', 70, 100.00);
+INSERT INTO Producto VALUES ('prod11',  'Janinge silla', 'Gracias a la flexibilidad del asiento y el respaldo, resulta muy cómoda.', 20, 100.00);
+INSERT INTO Producto VALUES ('prod12',  'Tobias silla', 'It just works!', 50, 100.00);
+
+GRANT ALL PRIVILEGES ON Producto TO UO245303;
+GRANT ALL PRIVILEGES ON Producto TO UO245303;
+GRANT ALL PRIVILEGES ON Producto TO UO245303;
