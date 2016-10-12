bd9bf3440f3c4afb939a40d5eacbd219728df66f
Pedro <pedrofernandezalvarez6@gmail.com>
2016-10-12 20:05:51 +0200

    Comienzo de las clases Almacenero y OrdenDeTrabajo

--- IPS-Project/IpsApp/src/logica/Almacenero.java
+++ IPS-Project/IpsApp/src/logica/Almacenero.java
@@ -1,5 +1,36 @@
 package logica;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Almacenero {
+	
+	String id;
+	String password;
+	String nombre;
+	String apellidos;
+	List<OrdenDeTrabajo> ordenesDeTrabajo = new ArrayList<OrdenDeTrabajo>();
+	
+	public void procesarOrdenDeTrabajo(OrdenDeTrabajo ordenDeTrabajo) {
+		boolean incidencia = prepararPaquete(ordenDeTrabajo);
+		if(!incidencia) {
+			ordenDeTrabajo.marcarParaEmpaquetar();
+		}
+		else {
+			ordenDeTrabajo.anotarIncidencia();
+		}
+	}
+	
+	
+	/** El almacenero recorre el almacén preparando el paquete
+	 * @param ordenDeTrabajo
+	 * @return true si ha habido incidencia, false si todo ha ido bien
+	 */
+	public boolean prepararPaquete(OrdenDeTrabajo ordenDeTrabajo) {
+		
+		return false;
+	}
+	
+
 
 }
--- IPS-Project/IpsApp/src/logica/OrdenDeTrabajo.java
+++ IPS-Project/IpsApp/src/logica/OrdenDeTrabajo.java
@@ -1,10 +1,22 @@
 package logica;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class OrdenDeTrabajo {
 	
 	private Almacenero almacenero;
 	private Pedido pedido;
 	private String estado;
+	private List<Producto> productosRecogidos = new ArrayList<Producto>();
+	
+	public void marcarParaEmpaquetar() {
+		
+	}
+	
+	public void anotarIncidencia() {
+		
+	}
 	
 	
 
